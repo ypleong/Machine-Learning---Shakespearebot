456917866b38bcdc0f8f676f352d0ca72670495e
ypleong <ypleong@datascopeanalytics.com>
2017-02-19 21:11:00 -0800

    save before changing all to hmmlearn

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,25 +3,69 @@ from nltk.tag.hmm import demo_bw, _create_hmm_tagger, HiddenMarkovModelTrainer
 import numpy as np
 import random
 import time
+from hmmlearn.hmm import MultinomialHMM
 
-all_lines = []
-all_words = set()
-all_poems = []
-poem_temp = []
-with open('../project2data/shakespeare.txt') as f:
-    for line in f.readlines():
-        line_tokens = word_tokenize(line)
 
-        if len(line_tokens) > 1:
-            all_words.update(line_tokens)
-            all_lines.append(line_tokens)
-            poem_temp += line_tokens
-        elif len(line_tokens) == 1:
-            all_poems.append(poem_temp)
-            poem_temp = []
+def process_data(filename):
+    all_lines = []
+    all_words = set()
+    all_poems = []
+    poem_temp = []
+    with open(filename) as f:
+        for line in f.readlines():
+            line_tokens = [word.lower() for word in word_tokenize(line)]
+
+            if len(line_tokens) > 1:
+                all_words.update(line_tokens)
+                all_lines.append(line_tokens)
+                poem_temp += line_tokens
+            elif len(line_tokens) == 1:
+                all_poems.append(poem_temp)
+                poem_temp = []
 
     all_poems.append(poem_temp)
     all_poems = all_poems[1:]
+
+    return all_words, all_poems, all_lines
+
+
+def create_random_matrix(L, D):
+    """return a LxD matrix as a list of list"""
+    A = [[random.random() for _ in range(D)] for _ in range(L)]
+
+    for i in range(len(A)):
+        norm = sum(A[i])
+        for j in range(len(A[i])):
+            A[i][j] /= norm
+
+    return A
+
+
+class HiddenMarkovModelTrainerSubClass(HiddenMarkovModelTrainer):
+    def __init__(self, states=None, symbols=None):
+        HiddenMarkovModelTrainer.__init__(self, states=states, symbols=symbols)
+
+    def transition_matrix(self):
+        trans_iter = (self._transitions[sj].prob(si)
+                      for sj in self._states
+                      for si in self._states)
+
+        transitions_prob = np.fromiter(trans_iter, dtype=np.float64)
+        N = len(self._states)
+        return transitions_prob.reshape((N, N)).T
+
+    def observation_matrix(self):
+        trans_iter = (self._outputs[sj].prob(si)
+                      for sj in self._symbols
+                      for si in self._states)
+
+        transitions_prob = np.fromiter(trans_iter, dtype=np.float64)
+        N = len(self._states)
+        M = len(self._symbols)
+        return transitions_prob.reshape((N, M)).T
+
+
+all_words, all_poems, all_lines = process_data('../project2data/shakespeare.txt')
 
 states = range(100)
 symbols = list(all_words)
@@ -30,21 +74,10 @@ L = len(states)
 D = len(symbols)
 
 # Randomly initialize and normalize matrix A.
-# random.seed(100)
-A = [[random.random() for i in range(L)] for j in range(L)]
-
-for i in range(len(A)):
-    norm = sum(A[i])
-    for j in range(len(A[i])):
-        A[i][j] /= norm
+A = create_random_matrix(L, L)
 
 # Randomly initialize and normalize matrix O.
-O = [[random.random() for i in range(D)] for j in range(L)]
-
-for i in range(len(O)):
-    norm = sum(O[i])
-    for j in range(len(O[i])):
-        O[i][j] /= norm
+O = create_random_matrix(L, D)
 
 pi = [1. / L for _ in range(L)]
 
@@ -54,7 +87,7 @@ training = []
 for line in all_poems:
     training.append([(i, None) for i in line])
 
-trainer = HiddenMarkovModelTrainer(states, symbols)
+trainer = HiddenMarkovModelTrainerSubClass(states, symbols)
 curr_time = time.time()
 hmm = trainer.train_unsupervised(training, model=model,
                                  max_iterations=1000)
