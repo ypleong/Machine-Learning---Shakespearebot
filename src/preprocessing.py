f4160a81ef34ba871667671c84b07f92dd9bb214
ypleong <ypleong@datascopeanalytics.com>
2017-02-19 23:27:05 -0800

    add some functions for post processing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,7 +2,9 @@ from nltk.tokenize import word_tokenize
 from nltk.tag.hmm import _create_hmm_tagger, HiddenMarkovModelTrainer
 import numpy as np
 import random
+import networkx as nx
 import time
+import matplotlib.pyplot as plt
 
 
 def process_data(filename):
@@ -29,7 +31,7 @@ def process_data(filename):
 
 
 def create_random_matrix(L, D):
-    """return a LxD matrix as a list of list"""
+    """return a LxD matrix"""
     A = [[random.random() for _ in range(D)] for _ in range(L)]
 
     for i in range(len(A)):
@@ -47,7 +49,7 @@ def transition_matrix(self):
 
     transitions_prob = np.fromiter(trans_iter, dtype=np.float64)
     N = len(self._states)
-    return transitions_prob.reshape((N, N)).T
+    return transitions_prob.reshape((N, N))
 
 
 def observation_matrix(self):
@@ -58,12 +60,78 @@ def observation_matrix(self):
     transitions_prob = np.fromiter(trans_iter, dtype=np.float64)
     N = len(self._states)
     M = len(self._symbols)
-    return transitions_prob.reshape((N, M)).T
+    return transitions_prob.reshape((N, M))
 
+
+def visualize_transition_matrix_graph(A):
+    G = nx.DiGraph()
+    Asize = A.shape
+    G.add_nodes_from(range(Asize[0]))
+
+    edge_labels = {}
+
+    for i in range(Asize[0]):
+        for j in range(Asize[1]):
+            G.add_edge(i, j, weight=A[i, j])
+            edge_labels[(i, j)] = '{:.4f}'.format(A[i, j])
+
+    pos = nx.spectral_layout(G)  # positions for all nodes
+
+    # nodes
+    nx.draw_networkx_nodes(G, pos, node_size=700)
+
+    # edges
+    nx.draw_networkx_edges(G, pos, width=2)
+
+    # labels
+    nx.draw_networkx_labels(G, pos, font_size=20, font_family='sans-serif')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=12, font_family='sans-serif')
+
+    plt.axis('off')
+    plt.savefig("weighted_graph.png")  # save as png
+    plt.show()  # display
+
+
+def plot_observation_bar(O):
+    Osize = O.shape
+
+    f, ax = plt.subplots(Osize[0], sharex=True, sharey=True)
+
+    for ind in range(len(ax)):
+        # print ind
+        ax[ind].scatter(range(Osize[1]), O[ind], marker='.')
+    # plt.setp(markerline, 'linewidth', 1)
+
+    ax[0].set_title('Sharing both axes')
+    # Fine-tune figure; make subplots close to each other and hide x ticks for
+    # all but bottom plot.
+    f.subplots_adjust(hspace=0)
+    plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
+    plt.xlim(0, Osize[1])
+    # plt.ylim(0, None)
+
+    plt.savefig("observation_graph.png")  # save as png
+    plt.show()  # display
+
+
+def find_top_words_for_states(n, O, symbols):
+    """
+    Top n words for each state.
+    Return dictionary with the state as the key
+    and a list of tuple (word, probability) as the value
+    """
+    Osize = O.shape
+
+    top_words = {}
+    for state in range(Osize[0]):
+        top_index = sorted(range(len(O[state])), key=lambda i: O[state, i], reverse=True)[:n]
+        top_words[state] = [(symbols[ind], O[state, ind]) for ind in top_index]
+
+    return top_words
 
 all_words, all_poems, all_lines = process_data('../project2data/shakespeare.txt')
 
-states = range(10)
+states = range(100)
 symbols = list(all_words)
 
 L = len(states)
@@ -71,9 +139,11 @@ D = len(symbols)
 
 # Randomly initialize and normalize matrix A.
 A = create_random_matrix(L, L)
+# visualize_transition_matrix_graph(A)
 
 # Randomly initialize and normalize matrix O.
 O = create_random_matrix(L, D)
+find_top_words_for_states(10, O, symbols)
 
 pi = [1. / L for _ in range(L)]
 
@@ -88,4 +158,5 @@ curr_time = time.time()
 hmm = trainer.train_unsupervised(training, model=model,
                                  max_iterations=1)
 print time.time() - curr_time
-print hmm
+
+plot_observation_bar(observation_matrix(hmm))
