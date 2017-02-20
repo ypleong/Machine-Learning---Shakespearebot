c8f6fddd8f2e5cfb4860e624dba188dc20e5519e
ypleong <ypleong@datascopeanalytics.com>
2017-02-19 18:03:28 -0800

    initial hmm code

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,27 +1,53 @@
 from nltk.tokenize import word_tokenize
-from nltk.tag.hmm import demo_bw, _create_hmm_tagger
+from nltk.tag.hmm import demo_bw, _create_hmm_tagger, HiddenMarkovModelTrainer
 import numpy as np
+import random
 
 all_lines = []
 all_words = set()
 with open('../project2data/shakespeare.txt') as f:
     for line in f.readlines():
         line_tokens = word_tokenize(line)
-        if len(line_tokens) > 0:
+        if len(line_tokens) > 1:
             all_lines.append(line_tokens)
 
         if len(line_tokens) > 1:
             all_words.update(line_tokens)
 
+states = range(100)
+symbols = list(all_words)
 
-print line
+L = len(states)
+D = len(symbols)
 
-# states = ['bull', 'bear', 'static']
-# symbols = ['up', 'down', 'unchanged']
-# A = np.array([[0.6, 0.2, 0.2], [0.5, 0.3, 0.2], [0.4, 0.1, 0.5]], np.float64)
-# B = np.array([[0.7, 0.1, 0.2], [0.1, 0.6, 0.3], [0.3, 0.3, 0.4]], np.float64)
-# pi = np.array([0.5, 0.2, 0.3], np.float64)
-#
-# model = _create_hmm_tagger(states, symbols, A, B, pi)
+# Randomly initialize and normalize matrix A.
+# random.seed(100)
+A = [[random.random() for i in range(L)] for j in range(L)]
 
+for i in range(len(A)):
+    norm = sum(A[i])
+    for j in range(len(A[i])):
+        A[i][j] /= norm
+
+# Randomly initialize and normalize matrix O.
+O = [[random.random() for i in range(D)] for j in range(L)]
+
+for i in range(len(O)):
+    norm = sum(O[i])
+    for j in range(len(O[i])):
+        O[i][j] /= norm
+
+pi = [1. / L for _ in range(L)]
+
+model = _create_hmm_tagger(states, symbols, A, O, pi)
+
+training = []
+for line in all_lines:
+    training.append([(i, None) for i in line])
+
+trainer = HiddenMarkovModelTrainer(states, symbols)
+hmm = trainer.train_unsupervised(training, model=model,
+                                 max_iterations=1000)
+
+print hmm
 # demo_bw()
