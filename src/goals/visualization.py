d171aa4e8160effff7e7df8deb5f71bd4482480a
ypleong <ypleong@datascopeanalytics.com>
2017-02-26 00:41:23 -0800

    add more visualization code

--- src/goals/visualization.py
+++ src/goals/visualization.py
@@ -1,16 +1,14 @@
-from nltk.tokenize import word_tokenize
-from nltk.tag.hmm import HiddenMarkovModelTagger, HiddenMarkovModelTrainer
-from nltk.probability import (DictionaryConditionalProbDist,
-                              RandomProbDist)
 import numpy as np
-import random
-import time
 import pickle
 import matplotlib.pyplot as plt
+import networkx as nx
+import operator
+
 
 def load_obj(name):
     with open(name + '.pkl', 'rb') as f:
         return pickle.load(f)
+
 
 def visualize_transition_matrix_graph(A):
     G = nx.DiGraph()
@@ -41,26 +39,64 @@ def visualize_transition_matrix_graph(A):
     plt.show()  # display
 
 
-def plot_observation_bar(O):
+def plot_matrix(A, O, word_dict, normalize=True):
     Osize = O.shape
-    
-    f, ax = plt.subplots(Osize[0], sharex=True, sharey=True)
-    
-    for ind in range(len(ax)):
-        # print ind
-        ax[ind].scatter(range(Osize[1]), O[ind], marker='.')
-    # plt.setp(markerline, 'linewidth', 1)
-    
-    ax[0].set_title('Sharing both axes')
-    # Fine-tune figure; make subplots close to each other and hide x ticks for
-    # all but bottom plot.
-    f.subplots_adjust(hspace=0)
-    plt.setp([a.get_xticklabels() for a in f.axes[:-1]], visible=False)
-    plt.xlim(0, Osize[1])
-    # plt.ylim(0, None)
-    
+    Onew = O
+    Anew = A
+
+    if normalize:
+        Onew = np.zeros(Osize)
+        Anew = np.zeros(A.shape)
+        for row in range(Osize[0]):
+            Onew[row, :] = O[row, :]/max(O[row, :])
+            Anew[row, :] = A[row, :]/max(A[row, :])
+
+    # plt.imshow(Onew, aspect='auto', cmap='magma', interpolation='nearest')
+    # plt.colorbar(orientation='horizontal', aspect=100)
+    # plt.clim(vmin=0, vmax=1)
+    # plt.tight_layout()
+    # plt.savefig("observation_graph.png")  # save as png
+    # plt.show()
+
+
+    fig, ax1 = plt.subplots(1, 1)
+    # cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    ax1.imshow(Onew[:, :100], aspect='auto', cmap='magma', interpolation='nearest', vmin=0.0, vmax=1.0)
+    # fig.colorbar(ax1, cax=cbar_ax)
+    ax1.set_xticks(range(100))
+    ax1.set_xticklabels(word_dict[:100], rotation=90)
+    # ax1.tight_layout()
     plt.savefig("observation_graph.png")  # save as png
     plt.show()  # display
+
+    # plt.imshow(Onew[:, 1000:2000], extent=[1000, 1999, 19, 0], aspect='auto', cmap='magma', interpolation='nearest')
+    # plt.colorbar(orientation='horizontal', aspect=100)
+    # plt.clim(vmin=0, vmax=1)
+    # plt.tight_layout()
+    # plt.savefig("observation_graph.png")  # save as png
+    # plt.show()
+
+    # plt.imshow(Onew[:, 2000:3000], extent=[2000, 2999, 19, 0], aspect='auto', cmap='magma',
+    #            interpolation='nearest')
+    # plt.colorbar(orientation='horizontal', aspect=100)
+    # plt.clim(vmin=0, vmax=1)
+    # plt.tight_layout()
+    # plt.savefig("observation_graph.png")  # save as png
+    # plt.show()
+    #
+    # plt.imshow(Onew[:, 3000:], extent=[3000, Osize[1], 19, 0], aspect='auto', cmap='magma',
+    #            interpolation='nearest')
+    # plt.colorbar(orientation='horizontal', aspect=20)
+    # plt.clim(vmin=0, vmax=1)
+    # plt.tight_layout()
+    # plt.savefig("observation_graph.png")  # save as png
+    # plt.show()
+
+    plt.matshow(A, aspect='auto', cmap='magma')
+    plt.colorbar()
+    plt.savefig("transition_graph.png")  # save as png
+    plt.show()
+
 
 def find_top_words_for_states(n, O, symbols):
     """
@@ -71,11 +107,23 @@ def find_top_words_for_states(n, O, symbols):
     Osize = O.shape
 
     top_words = {}
+    top_words_prob = {}
     for state in range(Osize[0]):
-        top_index = sorted(range(len(O[state])), key=lambda i: O[state, i], reverse=True)[:n]
-        top_words[state] = [(symbols[ind], O[state, ind]) for ind in top_index]
+        top_index = sorted(range(len(O[state])), key=lambda i: O[state, i], reverse=True)
+        top_words[state] = [(symbols[ind], O[state, ind]) for ind in top_index[0:n]]
 
-    return top_words
+        curr_prob = O[state, top_index[0]]
+        top_words_prob_temp = []
+        ind = 0
+        while curr_prob < 0.5:
+            top_ind = top_index[ind]
+            top_words_prob_temp.append((symbols[top_ind], O[state, top_ind]))
+            ind += 1
+            curr_prob += O[state, top_index[ind]]
+
+        top_words_prob[state] = top_words_prob_temp
+
+    return top_words, top_words_prob
 
 
 def save_obj(obj, name):
@@ -83,14 +131,46 @@ def save_obj(obj, name):
         pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
 
 
-A_list = load_obj('transition_matrix')
-O_list = load_obj('observation_matrix')
-symbols = load_obj('symbols')
+def count_words(all_lines):
+    words = {}
+    for line in all_lines:
+        for word in line:
+            if word in words:
+                words[word] += 1
+            else:
+                words[word] = 1
 
-A = np.array(A_list,dtype=np.float64)
-O = np.array(O_list,dtype=np.float64)
-top_10_words = find_top_words_for_states(10, O, symbols)
-save_obj(top_10_words, 'top_10_words')
-print(top_10_words)
-plot_observation_bar(O)
+    new_words = sorted(words.items(), key=operator.itemgetter(1), reverse=True)
+
+    return [item[0] for item in new_words]
+
+
+A_list = load_obj('./data/transition_matrix_line_20')
+O_list = load_obj('./data/observation_matrix_line_20')
+symbols = load_obj('symbols')
+word_dict = load_obj('word_dictionary')
+
+lines = load_obj('training_data')
+ordered_words_int = count_words(lines)
+
+A = np.array(A_list, dtype=np.float64)
+O = np.array(O_list, dtype=np.float64)
+
+O = O[:, ordered_words_int]
+ordered_words = [word_dict[ind] for ind in ordered_words_int]
+
+# plot_matrix(A, O, ordered_words)
+
+top_10_words, top_words_prob = find_top_words_for_states(10, O, symbols)
+
+for list in top_words_prob:
+    print len(top_words_prob[list])
+
+for state in top_10_words:
+    print str(state) + ' & ' + ' & '.join([item[0] for item in top_10_words[state]]) + ' \\\\'
+    print ' & ' + ' & '.join(['{:.4f}'.format(float(item[1])) for item in top_10_words[state]]) + ' \\\\ \\hline'
+
+# save_obj(top_10_words, 'top_10_words')
+# print(top_10_words)
+
 
